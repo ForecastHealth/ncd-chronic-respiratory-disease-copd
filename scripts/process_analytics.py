b2f5f35709eab529691c83bedee49c71c545d287
Rory Watts <rory@forecasthealth.org>
2025-08-25 10:18:54 +0800

    Added cases metric for cases averted

--- scripts/process_analytics.py
+++ scripts/process_analytics.py
@@ -115,6 +115,12 @@ METRICS_CONFIG = {
         "element_labels": ["DsFreeSus", "AsthmaEpsd"],
         "aggregation": "cost",
         "apply_discounting": True
+    },
+    "cases": {
+        "name": "cases",
+        "event_type": "echo",
+        "element_labels": ["COPDEpsd"],
+        "aggregation": "yearly"
     }
 }
 
